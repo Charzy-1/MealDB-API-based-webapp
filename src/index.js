2aeae98885cd7670bf48244325ab739e11171ca3
Charles <chazylites@gmail.com>
2024-08-06 12:40:31 +0100

    added some other files to make my app work

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// index.js
+import '../styles.css'; // Adjust the path as necessary
+
+// Your JavaScript code here
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/ui.js', 
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public/dist'),
+    filename: 'bundle.js', 
   },
   module: {
     rules: [
@@ -17,7 +17,5 @@ module.exports = {
       },
     ],
   },
-  devServer: {
-    contentBase: './dist',
-  },
+  devtool: 'source-map', // Helps with debugging
 };
